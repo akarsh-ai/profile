d8c0dbb2e4317411b72b9c36747524b6ccb6494f
Akarsh Bharadwaj <151930146+akarsh-bh@users.noreply.github.com>
2024-05-15 22:17:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for additional effects can be added here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document is ready');
+    // You can add more interactive features here
+});
